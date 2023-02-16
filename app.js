7c9bab8fc17fe2314f8a051cc9a72cd4a83cdf35
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-16 20:09:38 +0000

    Added route for convert API

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var apiRoutes = require('./routes/api.js');
+
 // serve static assets from the 'public' directory
 app.use(express.static('public'))
+
+//Routing for API 
+apiRoutes(app);
 
 // listen for requests
 app.listen(port, () => {
