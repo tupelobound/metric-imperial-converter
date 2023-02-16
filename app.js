0b8c42ee94b42fcccceab082005ff7d8ff6e63b1
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-16 21:42:22 +0000

    Added freeCodeCamp test functionality

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const apiRoutes = require('./routes/api.js')
+// const fccTestingRoutes  = require('./routes/fcctesting.js')
 const runner = require('./test-runner')
+const helmet = require('helmet')
+
+app.use(helmet()) // use helmet security
+
+// app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 // serve static assets from the 'public' directory
 app.use(express.static('public'))
+
+//For FCC testing purposes
+// fccTestingRoutes(app);
 
 //Routing for API 
 apiRoutes(app);
